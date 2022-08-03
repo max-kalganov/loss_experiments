6532079de1423c8f67218b265f4dd1f0cfafa2a6
max <maksim.kalganov@gmail.com>
2022-08-03 19:34:31 +0300

    add kernel_initializer for model experiments

--- model.py
+++ model.py
@@ -1,12 +1,18 @@
 """Implements simple model with one layer and two nodes in it"""
+from typing import Optional, List
+
 import tensorflow as tf
 import gin
 
 
 @gin.configurable()
-def get_model(optimizer, loss, metric):
+def get_model(optimizer, loss, metric, kernel_init: Optional[List[float]] = None):
     x_input = tf.keras.layers.Input(2, dtype=tf.float32)
-    x = tf.keras.layers.Dense(1, activation='sigmoid')(x_input)
+    kernel_init = kernel_init if kernel_init is None else tf.constant_initializer(kernel_init)
+    x = tf.keras.layers.Dense(1,
+                              activation='sigmoid',
+                              kernel_initializer=kernel_init,
+                              bias_initializer=tf.keras.initializers.Zeros())(x_input)
     model = tf.keras.models.Model(inputs=[x_input], outputs=[x])
     model.compile(optimizer=optimizer,
                   loss=loss,
