0da4108fa8023ebaed52538fa81cb47e6a6b61fb
max <maksim.kalganov@gmail.com>
2022-08-03 21:25:31 +0300

    remove default type (caused errors when training model on mac)

--- model.py
+++ model.py
@@ -3,11 +3,11 @@ from typing import Optional, List
 
 import tensorflow as tf
 import gin
-tf.keras.backend.set_floatx('float64')
+# tf.keras.backend.set_floatx('float64')
 tf.config.set_soft_device_placement(True)
 
 @gin.configurable()
-def get_model(optimizer, loss, metric, kernel_init: Optional[List[float]] = None):
+def get_model(optimizer, loss, metrics, kernel_init: Optional[List[float]] = None):
     x_input = tf.keras.layers.Input(2, dtype=tf.float64)
     kernel_init = kernel_init if kernel_init is None else tf.keras.initializers.Constant(kernel_init)
     x = tf.keras.layers.Dense(1,
@@ -17,5 +17,5 @@ def get_model(optimizer, loss, metric, kernel_init: Optional[List[float]] = None
     model = tf.keras.models.Model(inputs=[x_input], outputs=[x])
     model.compile(optimizer=optimizer,
                   loss=loss,
-                  metrics=[metric])
+                  metrics=metrics)
     return model
